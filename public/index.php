7b7878ad0aab39182197f4aee4bf5b22609a1a6d
Max Antipin <max.v.antipin@gmail.com>
2026-01-13 18:51:21 +0300

    Add Twig

--- public/index.php
+++ public/index.php
@@ -7,12 +7,20 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
 
 $app = AppFactory::create();
 
+$twig = Twig::create(__DIR__ . '/../templates', ['cache' => __DIR__ . '/../var/cache']);
+
+$app->add(TwigMiddleware::create($app, $twig));
+
 $app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
-    return $response;
+    $view = Twig::fromRequest($request);
+    return $view->render($response, 'main.html.twig', [
+        'text' => 'Hello world!'
+    ]);
 });
 
 $app->run();
