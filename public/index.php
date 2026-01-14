a776d57483195ebc2c705810cb48f56217988d17
Max Antipin <max.v.antipin@gmail.com>
2026-01-14 03:37:50 +0300

    Add GuzzleHttp

--- public/index.php
+++ public/index.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
@@ -44,7 +46,18 @@ $app->post('/urls', function (Request $request, Response $response, $args): Resp
         $url = $params['url']['name'];
         $isUrl = static fn (string $u): bool => ($u = parse_url($u)) && !empty($u['host']) && isset($u['scheme']) && isset(['http' => 1, 'https' => 1][$u['scheme']]);
         // filter_var($params['url']['name'], FILTER_VALIDATE_URL) // не работает с национальными URL (https://дом.рф)
-        $response->getBody()->write($isUrl($url) ? $url : 'Invalid URL');
+        if ($isUrl($url)) {
+            $client = new Client();
+            try {
+                $r = $client->get($url);
+                $text = $url . ' ' . $r->getStatusCode();
+            } catch (RequestException $e) {
+                $text = $e->getMessage();
+            }
+        } else {
+            $text = 'Invalid URL';
+        }
+        $response->getBody()->write($text);
     }
     return $response;
 });
